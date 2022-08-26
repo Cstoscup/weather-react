eba9ffe5460ff75d2abfa0e85d570ec40b04cf6a
Cstoscup <109521820+Cstoscup@users.noreply.github.com>
2022-08-26 16:31:00 -0400

    Added github and portfolio links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <Card city="New York" />
+      <p><a href="https://github.com/Cstoscup/weather-react" target="_blank" rel="noreferrer">Open-source</a> code, by <a href="https://snazzy-raindrop-534df3.netlify.app" target="_blank" rel="noreferrer">Callie Stoscup</a></p>
     </div>
   );
 }
